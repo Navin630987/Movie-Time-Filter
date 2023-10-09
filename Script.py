07de5cba932f7ea72539fe484b98110e2d3111a7
Kumaresan J <145225330+Swethainiyan@users.noreply.github.com>
2023-10-09 17:10:46 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -221,8 +221,12 @@ Wᴇʟᴄᴏᴍᴇ ᴛᴏ {chat}.
 ꜱʜᴀʀᴇ & ꜱᴜᴩᴩᴏʀᴛ, ʀᴇqᴜᴇꜱᴛ ʏᴏᴜ ᴡᴀɴᴛᴇᴅ ᴍᴏᴠɪᴇꜱ"""
   
     IMDB_TEMPLATE = """
-    
-    Your File is Redy ✅ 👇
+    <b>Qᴜᴇʀʏ: {query}</b>
+
+🏷 Tɪᴛʟᴇ: <a href={url}>{title}</a>
+🎭 Gᴇɴʀᴇꜱ: {genres}
+📆 Yᴇᴀʀ: <a href={url}/releaseinfo>{year}</a>
+🌟 Rᴀᴛɪɴɢ: <a href={url}/ratings>{rating}</a>/10
     """
    
   
